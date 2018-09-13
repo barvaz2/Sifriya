0a4095bff69e9884269107045e57f33198fe3c57
Ofer Barasofsky <oferbarasofsky@gmail.com>
2018-09-14 00:28:40 +0300

    started tutorial

--- runserver.go
+++ runserver.go
@@ -10,7 +10,12 @@ func getAllCategoriesList() string {
   return "test123"
 }
 
-func getGategories(w http.ResponseWriter, r *http.Request) {  
+func handleFunc(w http.ResponseWriter, r *http.Request) {  
+  if r.URL.Path != "/" {
+    http.Redirect(w, r, "/", http.StatusFound)
+    return
+  }
+
   reqpath := strings.TrimPrefix(r.URL.Path, "/")
   sa := strings.Split(reqpath, "/")
   if len(sa) > 1 && sa[0] == "categories" && sa[1] == "list" {
@@ -26,16 +31,16 @@ func getGategories(w http.ResponseWriter, r *http.Request) {
 }
 func main() {
 
-  fs := http.FileServer(http.Dir("public"))
-  http.Handle("/", fs)
-  http.HandleFunc("/categories", getGategories)
+  //fs := http.FileServer(http.Dir("public"))
+  //http.Handle("/", fs)
+  http.HandleFunc("/", handleFunc)
   /*if err := http.ListenAndServeTLS(":443", "ssl_cert/myKey.crt", "ssl_cert/myKey.key", nil); err != nil {
     panic(err)
   }*/
 
-  if err := http.ListenAndServe(":3000", nil); err != nil {
+  /*if err := http.ListenAndServe(":3000", nil); err != nil {
     panic(err)
-  }
+  }*/
 
   appengine.Main()
 }
