e4329e35ad14de069058a289d09f0bc16db219de
Ofer Barasofsky <oferbarasofsky@gmail.com>
2018-09-13 23:19:58 +0300

    test ssl

--- runserver.go
+++ runserver.go
@@ -24,7 +24,7 @@ func main() {
   fs := http.FileServer(http.Dir("public"))
   http.Handle("/", fs)
   http.HandleFunc("/categories/", getGategories)
-  if err := http.ListenAndServe(":80", nil); err != nil {
+  if err := http.ListenAndServeTLS(":3000", "ssl_cert/myKey.crt", "ssl_cert/myKey.key" nil); err != nil {
     panic(err)
   }
 
