8af6b26f5d853379dd3b18f775f309b89c1c2eee
Ofer Barasofsky <oferbarasofsky@gmail.com>
2018-09-13 23:39:44 +0300

    test123

--- runserver.go
+++ runserver.go
@@ -1,32 +1,32 @@
 package main
 import (
   "net/http"
-  "strings"
+  //"strings"
   "google.golang.org/appengine"
 )
 
 func getAllCategoriesList() string {
-  return ""
+  return "test123"
 }
 
 func getGategories(w http.ResponseWriter, r *http.Request) {
-  message := r.URL.Path
+  /*message := r.URL.Path
   message = strings.TrimPrefix(message, "/")
   sa := strings.Split(message, "/")
-  if len(sa) > 1 && sa[1] == "list" {
+  if len(sa) > 1 && sa[0] == "categories" && sa[1] == "list" {
     message = getAllCategoriesList();
     w.Write([]byte(message))
-  }
-  
+  }*/
+  w.Write([]byte("test123"))
 }
 func main() {
 
-  fs := http.FileServer(http.Dir("public"))
-  http.Handle("/", fs)
-  http.HandleFunc("/categories/", getGategories)
-  if err := http.ListenAndServeTLS(":443", "ssl_cert/myKey.crt", "ssl_cert/myKey.key", nil); err != nil {
+  //fs := http.FileServer(http.Dir("public"))
+  //http.Handle("/", fs)
+  http.HandleFunc("/", getGategories)
+  /*if err := http.ListenAndServeTLS(":443", "ssl_cert/myKey.crt", "ssl_cert/myKey.key", nil); err != nil {
     panic(err)
-  }
+  }*/
 
   appengine.Main()
 }
